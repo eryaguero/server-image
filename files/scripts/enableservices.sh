cc70fc39ea616f4254a8616c69c3b52b0d4a5218
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-16 14:40:24 -0700

    Update enableservices.sh

--- files/scripts/enableservices.sh
+++ files/scripts/enableservices.sh
@@ -6,4 +6,4 @@ systemctl enable unbound.service
 systemctl enable bootstrap-time.service
 systemctl enable stunnel.service
 semanage port -a -t dns_port_t -p udp 5335
-sed -i 's/^AutomaticUpdatePolicy=stage$/AutomaticUpdatePolicy=apply/' /etc/rpm-ostreed.conf
+sed -i 's/AutomaticUpdatePolicy=stage/AutomaticUpdatePolicy=apply/' /etc/rpm-ostreed.conf
